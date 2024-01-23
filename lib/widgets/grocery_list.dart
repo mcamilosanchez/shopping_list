aac17692ca37e87faf4b4f4cde4ed6207009a560
CamiloSanchez <camilo1632@gmail.com>
2024-01-23 08:55:34 -0500

    Adding and Delete item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,29 +1,103 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() {
+    return _GroceryListState();
+  }
+}
+
+class _GroceryListState extends State<GroceryList> {
+  /* 215. Passing Data Between Screens */
+  final List<GroceryItem> _groceryItems = [];
+  /* 215. Passing Data Between Screens
+  Al momento de cerrar la pantalla NewItem por medio de .pop, llega la 
+  información del nuevo item. Para eso, debemos escribir async en el método 
+  _addItem() y await para recuperar los datos. newItem puede ser un VALOR NULO, 
+  ya que el usuario puede salir de la pantalla presionando la tecla atrás sin 
+  haber guardado */
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+    /* Entonces, como newItem puede ser nulo, realizamos lo siguiente: */
+    if (newItem == null) {
+      return;
+    }
+    /* Si se salta el condicional, es porque se tiene un nuevo elemento, 
+    entonces debe ser agregado a la _groceryItems. Podemos hacerlo de la 
+    siguiente manera:
+
+    _groceryItems = newItem;
+
+    Peor sería un error ya que _groceryItems en final. Por lo anterior, 
+    usaremos add() */
+    setState(() {
+      /* Llamamos setState ya que queremos ejecutar el método build de nuevo 
+      porque ahora quiero usar artículos de comestibles en mi UI*/
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removedItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('Uh oh ... nothing here!'),
+          SizedBox(height: 16),
+          Text('Try adding a new item')
+        ],
+      ),
+    );
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          /* 216. Final Challenge Solution */
+          key: ValueKey(_groceryItems[index].id),
+          onDismissed: (direction) {
+            _removedItem(_groceryItems[index]);
+          },
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
-      ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
           ),
-          trailing: Text(
-            groceryItems[index].quantity.toString(),
-          ),
-        ),
+        ],
       ),
+      body: content,
     );
   }
 }
---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  /* 213. Getting Form Access via a Global Key
+  La diferencia entre GlobalKey() y ValueKey(), es que la primera también nos da
+  fácil acceso al widget subyacente al que está conectado y asegura que si ese 
+  método build se ejecuta de nuevo, porque establecemos algún estado. Este 
+  widget de formulario no se RECONSTRUYE, en su lugar mantiene su estado interno, 
+  esto es importante que trabajaremos con ese estado, por ejemplo, este estado 
+  le dirá a Flutter si mostrar o no algunos errores de validación. En conclusión,
+  siempre que trabajemos con formularios, debemos manejar GlobalKeys.
+
+  Con esto añadido <FormState> le estoy diciendo a Flutter que el widget o el 
+  objeto al que se conectará esta GlobalKey será un objeto de FormState.
+  */
+  final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  void _saveItem() {
+    /* VIDEO #213. Getting Form Access via a Global Key
+    El objetivo de esta funcion es activar la validación. Lo podemos hacer ya
+    que _formKey está conectada al formulario y desde ella, podemos acceder a la
+    propiedad currentState (recordar que al poner ! estamos diciéndole a Dart 
+    que nunca será Null).
+    Al momento de presionar el botón, el formulario ya se habrá creado y la 
+    clave del formulario se habrá adjuntado.
+
+    validate() retorna un bool: true si todas las funciones del validador han 
+    pasado o false, si al menos una función del validador falló. De esta manera,
+    estamos habilitando las validaciones del formulario que podrían devolver 
+    mensajes de error . */
+    if (_formKey.currentState!.validate()) {
+      /* VIDEO #214. Extracting Entered Values
+    Al llamar el método guardar, se activará una función especial en todos estos
+    widgets de campo formulario dentro del formulario */
+      _formKey.currentState!.save();
+      /* 215. Passing Data Between Screens
+      Recordar que para cerrar esta pantalla y pasar los valores a dicha 
+      pantalla, usamos .pop y en el argumento debemos enviar la infomación */
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toString(),
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a new item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        /* VIDEO #209. The Form & TextFormField Widgets */
+        child: Form(
+          /* VIDEO #213. Getting Form Access via a Global Key
+          Para ejecutar todas la validaciones que realizamos en los campos de 
+          texto dentro del formulario, necesitaremos un acceso a esos campos por
+          medio de KEY */
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  /* VIDEO #212. Adding Validation Logic 
+                  Realizaremos una validación para el campo de texto */
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters';
+                  }
+                  /* Si esta función retorna un null, es porque la validación se
+                  realizó de manera correcta, es decir, el valor es válido */
+                  return null;
+                },
+                /* 214. Extracting Entered Values
+                Esta función recibe el valor que se introdujo en este campo de 
+                texto como argumento (value) y este será el valor en el momento 
+                en que se ejecute .save */
+                onSaved: (value) {
+                  /* Recordar que al escribir ! estamos 
+                  dejando claro que value nunca será nulo o se podría hacer de 
+                  la siguiente manera, pero sería redundante: */
+                  // if (value == null) {
+                  //   return;
+                  // }
+                  _enteredName = value!;
+                },
+              ), //instead of TextField()
+              Row(
+                /* Recordar que para que los hijos estén alineados entre sí, 
+                usamos la propiedad crossAxisAlignment*/
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  /* VIDEO #210. A Form-aware Dropdown Button 
+                  Si debajo de este children, ubicamos un TextFormField habría 
+                  un error de renderización, ya que al final estamos usando un 
+                  campo de texto en lugar de una fila. Recordar que el campo de 
+                  texto es un unconstrained horizontalmente, por eso el error. 
+                  Se soluciona usando Expanded. */
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      keyboardType: TextInputType.number,
+                      initialValue: _enteredQuantity.toString(),
+                      validator: (value) {
+                        /* VIDEO #212. Adding Validation Logic 
+                        Realizaremos una validación para el campo de texto */
+                        if (value == null ||
+                            value.isEmpty ||
+                            /* tryParse retorna un null si falla al convertir la
+                            cadena a un número */
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid, positive number';
+                        }
+                        /* Si esta función retorna un null, es porque la 
+                        validación se realizó de manera correcta, es decir, el 
+                        valor es válido */
+                        return null;
+                      },
+                      onSaved: (value) {
+                        /* La diferencia entre parse y tryParse es que parse 
+                        arrojará un error si no consigue convertir la cadena, 
+                        mientras que tryParse devuelve un null. Igualmente, esta
+                        verificación la realizamos en el condicional anterior */
+                        _enteredQuantity = int.parse(value!);
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      value: _selectedCategory,
+                      items: [
+                        /* VIDEO #210. A Form-aware Dropdown Button
+                      Aquí vamor a realizar un loop (for) para recorrer una 
+                      lista, pero categories no es una lista, es un map. Por lo 
+                      cual, usaremos entries, es una propiedad proporcionada por 
+                      Flutter en cada map, entries le da un ITERABLE que al 
+                      final contiene todos los pares clave-valor de su mapa como 
+                      elementos en ese iterable. */
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {
+                        /* VIDEO #214. Extracting Entered Values
+                        Es necesario restablecer el estado ya que la categoría 
+                        seleccionada se utiliza para establecer el valor visible
+                        actualmente y por supuesto, debe permanecer en sincronía
+                        con lo que hemos seleccionado en el menú desplegable y 
+                        debe actualizarse cada vez que cambiamos nuestra 
+                        selección para que el valor seleccionado actualmente se 
+                        refleja en la pantalla */
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                /* Empujamos todos los botones hacía la derecha */
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveItem,
+                    child: const Text('Add item'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
